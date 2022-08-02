58835b9eac7a8fdf9cf1dbd67b3c2f5b015696df
Apple <apple@iMac-de-Apple.bwrouter>
2022-08-02 11:14:29 -0500

    suma de todos valores

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
+// Responsabilidad del archivo: acceder al dom
 
-console.log(validator);
+// acceso al dom
+const creditCardNumber = document.getElementById("cardNumber")
+const validar= document.getElementById("validar")
+
+
+validar.addEventListener("click",function(){
+    validator.isValid(creditCardNumber.value)// invocamos la funcion
+})
+ 
+
+
+   
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
-const validator = {
+// responsabilidad: tener toda la logica
+
+const validator = { //objeto
   // ...
-};
+  isValid: function (numbers) { // declaracion de isvalid
+
+    const inverse = numbers.split("").reverse().map(Number);
+    let plus = 0
+
+    for (let i = 0; i < inverse.length; i++) {
+      if (i % 2 !== 0) {//posiciones pares el indice comienza en 1  
+        let multiply = inverse[i] * 2
+        console.log(multiply)
+        //entonces multiplicar numeros en posiciones que cumplieron con la condicion
+        if (multiply >= 10) {
+          let oneNumber = multiply - 9
+          console.log(oneNumber)
+          plus+=oneNumber
+        } else {
+          plus+=multiply
+        }
+      }
+      if (i%2===1) {
+        //console.log (inverse [i],i)
+        plus+=inverse[i]
+      } 
+    } 
+    console.log(plus)  
+    let valid= plus%10===0
+    console.log(valid)
+
+  }
+}
+
+
 
 export default validator;
+//1. Split divide datos en un array de tipo string 2. Reverse voltea de fin a principio 3.map convierte datos a valor numerico    
+    /* segundo paso buscar posiciones impares por que se comienza desde cero (algoritmo luhn pide posiciones pares) 
+    El bucle FOR se utiliza para repetir una o más instrucciones,for (inicialización; condición; actualización) {
+     sentencias a ejecutar en cada iteración}*/
